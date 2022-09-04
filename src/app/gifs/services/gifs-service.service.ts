2e4888d4807feb23139e7b1c82c3cb99c4e08772
ElideZavala <zavalavinagreelide@gmail.com>
2022-09-04 17:21:10 -0500

    new elements

--- src/app/gifs/services/gifs-service.service.ts
+++ src/app/gifs/services/gifs-service.service.ts
@@ -7,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class GifsServiceService {
 
   private apiKey: string = 'qmxmoXa2i2pWNbrCpLmaskV21VzDFg8y'
-  private url: string = `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=dragon ball z&limit=10`
   private _historial: string[] = [];
+
+  // TODO: Cambiar any por su tipo.
+  public resultados: any[] = [];
 
   get historial() {
 
@@ -21,21 +23,16 @@ export class GifsServiceService {
 
 
   buscarGifs(query: string) {
-
-    // Almacenar todo en minusculas
     query = query.trim().toLowerCase();
-
-    // buscar si en el array de historial ya no se encuentra el nuevo query.
     if (!this._historial.includes(query)) {
-      // agregaremos el valor al arreglo en la primera posicion.
       this._historial.unshift(query);
-      // Cortar el arreglo principal del 0 al 10
       this._historial = this._historial.splice(0, 10);
     }
 
-    this.http.get(this.url)
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
       .subscribe((res: any) => {
-        console.log(res);
+        console.log(res.data);
+        this.resultados = res.data;
       })
   }
 
